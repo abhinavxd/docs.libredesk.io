12d209d3f295df1b824574eccd0df0a009ccad51
Abhinav Raut <abhinavrautcs@gmail.com>
2026-09-18 00:15:38 +0530

    add libredesk chat widget to all docs pages

---
+++ widget.js
@@ -0,0 +1,9 @@
+window.LibredeskSettings = {
+  baseURL: 'https://ld.libredesk.io',
+  inboxID: '63a318b7-c3c5-49c1-bfc1-24699b708464'
+};
+
+const s = document.createElement('script');
+s.src = 'https://ld.libredesk.io/widget.js';
+s.async = true;
+document.body.appendChild(s);
